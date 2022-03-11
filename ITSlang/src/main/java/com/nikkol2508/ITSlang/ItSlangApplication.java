9da6bce430c7926b02a63e90b0eb8ee2e5e02cf1
nikolajaksenov <aksenovnikk@yandex.ru>
2022-03-11 14:51:52 +0300

    project structure

---
+++ ITSlang/src/main/java/com/nikkol2508/ITSlang/ItSlangApplication.java
@@ -0,0 +1,13 @@
+package com.nikkol2508.ITSlang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSlangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSlangApplication.class, args);
+	}
+
+}
