8837f5646299b8f3b25c5756cb997ac653a3fe57
nikolajaksenov <aksenovnikk@yandex.ru>
2022-03-15 11:19:32 +0300

    echo bot refactor

--- ITSlang/src/main/java/com/nikkol2508/ITSlang/BotService.java
+++ ITSlang/src/main/java/com/nikkol2508/ITSlang/BotService.java
@@ -1,5 +1,6 @@
 package com.nikkol2508.ITSlang;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -9,14 +10,24 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Service
 public class BotService extends TelegramLongPollingBot {
+
+    private final String  botUsername;
+    private final String botToken;
+
+    public BotService(@Value("${telegram-bot.name}") String botUsername,
+                      @Value("${telegram-bot.token}") String botToken) {
+        this.botUsername = botUsername;
+        this.botToken = botToken;
+    }
+
     @Override
     public String getBotUsername() {
-        return "ITSlangTranslatorBot";
+        return botUsername;
     }
 
     @Override
     public String getBotToken() {
-        return "5280871293:AAHtabr2es4ylSP1TKOs-xDPlsdVuscNLT8" ;
+        return botToken ;
     }
 
     @Override
