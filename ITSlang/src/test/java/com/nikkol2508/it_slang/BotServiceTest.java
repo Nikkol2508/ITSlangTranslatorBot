ba5b7499707b347bb569a73ff69fc8913b68b66d
nikolajaksenov <aksenovnikk@yandex.ru>
2022-12-20 15:54:17 +0300

    added liquibase, amvera.yml.

--- ITSlang/src/test/java/com/nikkol2508/it_slang/BotServiceTest.java
+++ ITSlang/src/test/java/com/nikkol2508/it_slang/BotServiceTest.java
@@ -1,15 +1,28 @@
 package com.nikkol2508.it_slang;
 
+import com.nikkol2508.it_slang.service.BotService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@SpringBootTest
 class BotServiceTest {
+
+    @Autowired
+    private BotService botService;
 
     @Test
     void getBotUsername() {
+        Assertions.assertEquals("ITSlangTranslatorBot", botService.getBotUsername());
     }
 
     @Test
     void getBotToken() {
+        Assertions.assertEquals("5280871293:AAHtabr2es4ylSP1TKOs-xDPlsdVuscNLT8", botService.getBotToken());
     }
 
     @Test
