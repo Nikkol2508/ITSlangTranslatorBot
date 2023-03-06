dac33c8539ca39bf24ac6ec3123b1b0d6f361638
nikolajaksenov <aksenovnikk@yandex.ru>
2023-03-06 15:41:33 +0300

    Add github actions, little fix

--- src/main/java/com/nikkol2508/it_slang/repository/NotFoundRepository.java
+++ src/main/java/com/nikkol2508/it_slang/repository/NotFoundRepository.java
@@ -3,9 +3,11 @@ package com.nikkol2508.it_slang.repository;
 import com.nikkol2508.it_slang.repository.entity.NotFound;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface NotFoundRepository extends CrudRepository<NotFound, Integer> {
 
     @Query(value = "SELECT not_found_query AS notFoundQuery, count(id) AS countQuery FROM  not_found nf \n" +
--- src/main/java/com/nikkol2508/it_slang/repository/SlangRepository.java
+++ src/main/java/com/nikkol2508/it_slang/repository/SlangRepository.java
@@ -2,9 +2,11 @@ package com.nikkol2508.it_slang.repository;
 
 import com.nikkol2508.it_slang.repository.entity.SlangTranslator;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SlangRepository extends CrudRepository<SlangTranslator, Integer> {
 
     List<SlangTranslator> findTop5BySearchQueryContainingIgnoreCase(String searchQuery);
